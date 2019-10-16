b4f05802bbfc5cb580a10ada193ec42f040430d3
cmbahadir <c.mete.bahadir@gmail.com>
2019-10-16 20:46:31 +0300

    Get the data from DB.

--- get.py
+++ get.py
@@ -1,5 +1,7 @@
 from FR24Analyzer import FR24Analyzer
+# from FR24Analyzer.fit import fit
 import time
+
 
 # from apscheduler.schedulers.background import BackgroundScheduler
 # def gatherResults():
@@ -10,6 +12,8 @@ import time
 
 if __name__ == "__main__":
     analyzer = FR24Analyzer.FR24Analyzer()
+    # fitter = fit.Fit()
+    # fitter.getDataFromDB()
     while True:
         analyzer.getLandingTimeToAirport()
         time.sleep(2)
